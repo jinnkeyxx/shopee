c550d8181fdf9a07b8ecae77d37941e1337920e2
An Vo <vothaian97@gmail.com>
2020-09-29 19:38:59 +0700

    Update add status in Checkout model

--- application/models/Checkout_model.php
+++ application/models/Checkout_model.php
@@ -7,9 +7,11 @@ class Checkout_model extends CI_Model
   $this->db->from("users");
   if($query != '')
   {
-   $this->db->where('serial', $query);
-   $this->db->or_where('serial2', $query);
-   $this->db->or_where('serial3', $query);
+   // $this->db->where('serial', $query);
+   // $this->db->or_where('serial2', $query);
+   // $this->db->or_where('serial3', $query);
+   // $this->db->where('status', 0);
+   $this->db->where( 'status',  0)->group_start()->where('serial', $query)->or_where('serial2', $query)->or_where('serial3', $query)->group_end();
   }
   $this->db->order_by('id', 'DESC');
   return $this->db->get();
